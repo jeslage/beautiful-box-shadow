65a3128881b3d931b3cac181772b2614c1539e3d
Johannes Eslage <hello@johanneseslage.de>
2020-02-25 22:19:05 +0100

    Added document title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import App from "next/app";
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "../styles/global";
+import Head from "next/head";
 
 const theme = {
   backgroundDark: "#f6f9fc",
@@ -14,12 +15,17 @@ export default class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <ThemeProvider theme={theme}>
-        <>
-          <GlobalStyle />
-          <Component {...pageProps} />
-        </>
-      </ThemeProvider>
+      <>
+        <Head>
+          <title>Beautiful Box Shadow</title>
+        </Head>
+        <ThemeProvider theme={theme}>
+          <>
+            <GlobalStyle />
+            <Component {...pageProps} />
+          </>
+        </ThemeProvider>
+      </>
     );
   }
 }
